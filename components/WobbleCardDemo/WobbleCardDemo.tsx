d3f13dd2f399a1439346ce92c30d7539b0977982
alvee-sarker <asahanaz305@gmail.com>
2024-08-23 23:13:56 +0600

    changed font weight

--- components/WobbleCardDemo/WobbleCardDemo.tsx
+++ components/WobbleCardDemo/WobbleCardDemo.tsx
@@ -15,7 +15,7 @@ export function WobbleCardDemo() {
           <p className="mt-4 text-left text-[15px] text-neutral-200 sm:text-[18px]">
             Have an Idea You Want to Bring to Life?
           </p>
-          <h2 className="text-left text-balance text-[25px] sm:text-[30px] md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+          <h2 className="text-left geomatrixBold text-balance text-[25px] sm:text-[30px] md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Let's Make it Happen!
           </h2>
           <Link href={'/'} target="_black" className="w-auto h-auto px-10 py-3 mt-10 rounded-xl bg-transparent borderWhite z-10">Schedule a meeting</Link>
@@ -23,7 +23,7 @@ export function WobbleCardDemo() {
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 min-h-[300px] bg-black">
         <img src="/images/anik-sadik.jpg" className="h-20 rounded-full mb-6" alt="" />
-        <h2 className="max-w-80  text-left text-[25px] sm:text-[30px] text-base font-semibold tracking-[-0.015em] text-white">
+        <h2 className="max-w-80 text-left text-[25px] sm:text-[30px] text-base font-semibold tracking-[-0.015em] text-white">
           Anik Sadik Santo
         </h2>
         <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
