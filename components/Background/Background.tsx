6b0a50df2f0413272ce1da86c328126453c0c171
alvee-sarker <asahanaz305@gmail.com>
2024-08-25 09:52:22 +0600

    another change

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -9,7 +9,7 @@ export function GridBackgroundDemo() {
     <div className="h-[47rem] w-full dark:bg-black bg-black  dark:bg-grid-black/[0.2] bg-grid-white/[0.1] relative flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div>
+      <div className="w-[80%]">
         <AnimatedTooltipPreview />
         <HeroHighlightDemo />
         <div className="w-100% flex items-center justify-center mt-10 gap-4">
