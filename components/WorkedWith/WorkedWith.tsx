dd9a49cc612383fb6cf0f193fc6d21867ca44eb4
alvee-sarker <asahanaz305@gmail.com>
2024-09-11 23:01:13 +0600

    new updates

--- components/WorkedWith/WorkedWith.tsx
+++ components/WorkedWith/WorkedWith.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const WorkedWith = () => {
   return (
-    <div className='max-w-[52rem] w-[80%] bg-neutral-900 relative left-[50%] translate-x-[-50%] rounded-xl py-20 px-10 flex flex-col items-center justify-center gap-20 mt-40'>
-      <h1 className=' text-center geomatrixBold sm:text-4xl max-sm:text-3xl dark:text-white text-white'>Brands and Organigations we have worked with</h1>
-      <img src="/images/brand.png" className='md:w-96' alt="" />
+    <div className='max-w-[52rem] w-[80%] bg-neutral-900 relative left-[50%] translate-x-[-50%] rounded-xl py-20 px-10 flex flex-col items-center justify-center gap-16 mt-40'>
+      <h1 className=' text-center geomatrixBold sm:text-5xl max-sm:text-3xl dark:text-white text-white'>Brands and Organigations we have worked with</h1>
+      <img src="/images/brands.jpg" className='md:w-96' alt="" />
     </div>
   )
 }
